1a421dd25669f39fea4184b32f5502c9e7d7ccde
damcode1990 <damcode1990@gmail.com>
2022-11-29 13:55:34 +0530

    29-11-2022 1:55

---
+++ lib/Screens/patrika_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PatrikaScreen extends StatefulWidget {
+  const PatrikaScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PatrikaScreen> createState() => _PatrikaScreenState();
+}
+
+class _PatrikaScreenState extends State<PatrikaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
