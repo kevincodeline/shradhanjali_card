af3e326dfe014d07b9a9e63688b91c455d005a75
damcode1990 <damcode1990@gmail.com>
2022-11-30 12:35:53 +0530

    30-11-2022 12:35

---
+++ lib/Screens/panidhol_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PanidholScreen extends StatefulWidget {
+  const PanidholScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PanidholScreen> createState() => _PanidholScreenState();
+}
+
+class _PanidholScreenState extends State<PanidholScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
